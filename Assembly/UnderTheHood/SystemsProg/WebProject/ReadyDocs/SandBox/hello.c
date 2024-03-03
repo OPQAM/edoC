549b82f47d0329cddf6c7c2d4038c59a25bedd6d
OPQAM <theopqam@protonmail.com>
2024-03-03 16:37:33 +0000

    Many little changes as a result of creating webpage

---
+++ Assembly/UnderTheHood/SystemsProg/WebProject/ReadyDocs/SandBox/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void name();
+
+void hello() {
+	printf("Hello,");
+}
+
+int main(){
+	hello();
+	name();
+	return 0;
+}
---
+++ Assembly/UnderTheHood/SystemsProg/WebProject/ReadyDocs/SandBox/name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+void name() {
+	printf(" user!\n");
+}
