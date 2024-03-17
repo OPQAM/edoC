3564733ac32d466794fc337e2a8cbf4ff0eb6c32
OPQAM <theopqam@protonmail.com>
2024-03-17 16:32:40 +0000

    Feature: added some more code for inputing values into the matrices. Need to add controls and then multiply and present the matrices (re-write a 3rd matrix)
    Objective: Matrix multiplication app

--- Scripts/SimpleExercises/matrixMult.c
+++ Scripts/SimpleExercises/matrixMult.c
@@ -2,7 +2,9 @@
 
 int main() {
 	int rowsA, rowsB, columnsA, columnsB;
-
+	
+	//Stipulating matrix sizes
+	//Matrix A
 	printf("\nMatrix A\n");
 	printf("\nNumber of rows for Matrix A: ");
 	if (scanf("%d", &rowsA) != 1 || rowsA <= 0) {     // (1)
@@ -16,7 +18,9 @@ int main() {
 		return 1;
 	}
 	printf("\n\nMatrix A has %d rows and %d columns", rowsA, columnsA);
-	
+	int matrixA[rowsA][columnA];
+
+	//Matrix B
 	printf("\nMatrix B\n");
 	printf("\nNumber of rows for Matrix B: ");
 	if (scanf("%d", &rowsB) != 1 || rowsB <= 0) {     // (1)
@@ -30,12 +34,44 @@ int main() {
 		return 1;
 	}
 	printf("\n\nMatrix B has %d rows and %d columns", rowsB, columnsB);
+	int matrixB[rowsB][columnB];
 
 	// Multiplication Control
 	if (columnsA != rowsB) {
 		printf("Row multiplication not possible for current matrices.");
 		return 1;
 	}
+
+	//Populating Matrices
+	//Matrix A
+	
+	printf("\nLet's enter the values for matrix A.\n");
+	for (int i = 0; i < rowA; i++) {
+		for (int j = 0; j < columnA; j++) {
+			printf("\nEntry[%d][%d]: ",i, j)
+			scanf("%d",&matrixA[i][j]);
+		}
+	}
+	
+	printf("\nLet's enter the values for matrix B.\n");
+	for (int i = 0; i < rowB; i++) {
+		for (int j = 0; j < columnB; j++) {
+			printf("\nEntry[%d][%d]: ",i, j)
+			scanf("%d",&matrixB[i][j]);
+		}
+	}
+/*
+ * Consider something like:
+ *         printf("Entry [%d][%d]: ", i, j);
+            while (scanf("%d", &matrixA[i][j]) != 1) {
+                printf("Invalid input. Please enter an integer.\n");
+                // Clear input buffer to prevent infinite loop
+                while (getchar() != '\n');
+                printf("Entry [%d][%d]: ", i, j); // Reprompt
+ *
+ * Or subtract from i?.... check it out
+ * */
+
 }
 
 // NOTES:
