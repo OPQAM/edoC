bf93a51c8a6eab2f4cfb4c8550e61e7d21cc8066
OPQAM <theopqam@protonmail.com>
2024-04-16 11:53:51 +0100

    Feature: Added a getchar file
    Objective: C refreshing skills

---
+++ C/Intro101/l13_getchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(void){
+	char c;
+
+	c = getchar();
+
+	printf("%c\n",c);
+	// Note that 'putchar(c)' will also achieve the same effect and print the character of c.
+
+	return 0;
+
+}
