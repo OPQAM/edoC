6cd51468f60145c82937ee68e9117bdd6cf530f5
OPQAM <theopqam@protonmail.com>
2024-06-04 08:54:40 +0100

    Feature: wrote simple script example of a funct prototype
    Objective: Memory refresher on function prototypes

--- C/CLearning/04_functions101_beej.c
+++ C/CLearning/04_functions101_beej.c
@@ -13,3 +13,10 @@ int main(void)
 	
 	printf("i is %d and j is %d + 1, or %d\n", i, i, j);
 }
+
+
+// Note that we had to place the function 'add_one' before the main function.
+// But we don't have to do this. We can use function prototypes - 'oneliners' that are
+// placed at the beginning of the function and that will let the compiler know 
+// that specific function's name, return type, numbers and data types.
+// Open the script 04_functionPrototypes_beej.c
