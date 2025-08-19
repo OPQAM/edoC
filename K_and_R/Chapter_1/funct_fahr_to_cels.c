cb04897f0e2b399f69b12014076969edb2f68eec
OPQAM <theopqam@protonmail.com>
2025-08-19 17:54:27 +0100

    A couple more files for K&R's course

---
+++ K_and_R/Chapter_1/funct_fahr_to_cels.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+float celsiator(float fahr);
+
+int main(void)
+{
+    /* SCANF() */
+
+    int fahr;
+    
+    while (1)
+    {
+        printf("Please input a fahrenheit value (0-300): ");
+        scanf("%d", &fahr);
+        if (fahr >= 0 && fahr <= 300)
+        {
+            printf("Original value in fahrenheit: %d.\nCelsius conversion: %2.2f.\n", fahr, celsiator(fahr));
+            return 0;
+        } else {
+            printf("\nThat is not a valid input. Try again...\n");
+        }
+    }
+}
+
+float celsiator(float fahr)
+{
+    float result;
+    result = (5.0/9.0)*((fahr-32));
+    return result;
+}
