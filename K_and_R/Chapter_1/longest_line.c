5d8d9b364fecf10dab5ccb15eedacdfc693dc8a0
OPQAM <theopqam@protonmail.com>
2025-08-20 09:31:10 +0100

    slight code additions

--- K_and_R/Chapter_1/longest_line.c
+++ K_and_R/Chapter_1/longest_line.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-/* When we declare 'int getline(char line[], int maxline)', the compiler sees a conflict between our prototype and the library’s ssize_t getline(char **lineptr, size_t *n, FILE *stream). Hence we need to create a new name for our function. ie. instead of 'getline' we can call it 'get_line'.*/
+/* When we declare 'int getline(char line[], int maxline)', the compiler sees a conflict between our prototype and the library’s ssize_t getline(char **lineptr, size_t *n, FILE *stream). Hence we need to create a new name for our function. ie. instead of 'getline' we can call it 'get_line'.
+ * Also note that I'm using brackets on while loops. I think it makes the code clearer this way. I could be wrong.*/
+
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
 
@@ -15,13 +17,14 @@ int main(void)
 
     max = 0;
 
-    while ((len = get_line(line, MAXLINE)) > 0)
+    while ((len = get_line(line, MAXLINE)) > 0) { /* Side note: two-stepper here*/
         if (len > max) {
             max = len;
             copy(longest, line);
         }
     if (max > 0)
         printf("%s", longest);
+    }
     return 0;
 }
 
@@ -44,6 +47,7 @@ void copy(char to[], char from[])
     int i;
     i = 0;
 
-    while ((to[i] = from[i]) != '\0')
+    while ((to[i] = from[i]) != '\0') { /*Another two-stepper*/
         ++i;
+    }
 }
